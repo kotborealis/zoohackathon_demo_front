5e37b7995725a3d3f22ecbe96249f9c82815f6ba
kotborealis <kotborealis@gmail.com>
2019-11-16 14:22:48 +0300

    map prototype

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,22 +1,27 @@
 import React, {useState} from 'react';
 import {useApi} from '../../hooks/useApi';
-import ReactMapGL from 'react-map-gl';
+import ReactMapGL, {FullscreenControl} from 'react-map-gl';
+import styles from './index.css';
 
 export const Index = (props) => {
     const {data} = useApi(['/adduser']);
     const [mapState, setMapState] = useState({
-        width: 400,
-        height: 400,
+        width: '100%',
+        height: '100%',
         latitude: 37.7577,
         longitude: -122.4376,
         zoom: 8
     });
 
-    return (<div>
+    return (<div className={styles.container}>
         <ReactMapGL
             {...mapState}
             mapboxApiAccessToken={process.env.MAPBOX_API_TOKEN}
             onViewportChange={setMapState}
-        />
+        >
+            <div style={{position: 'absolute', right: 0}}>
+                <FullscreenControl container={document.querySelector('body')}/>
+            </div>
+        </ReactMapGL>
     </div>);
 };
